9166fa8dc18443ebdf0a262030675886bcdeef21
timint <info@tim-international.net>
2017-01-13 01:20:19 +0100

    * We don't need a special class for iframes. It's simply .fancybox-content > iframe

--- src/featherlight.js
+++ src/featherlight.js
@@ -204,10 +204,6 @@
 		/* sets the content of $instance to $content */
 		setContent: function($content){
 			var self = this;
-			/* we need a special class for the iframe */
-			if ($content.is('iframe') || $('iframe', $content).length > 0){
-				self.$instance.addClass(self.namespace+'-iframe');
-			}
 
 			self.$instance.removeClass(self.namespace+'-loading');
 
